6b006a36c72344d5d7724532a1599fb727ed43c2
zxl <43881818+oahzxl@users.noreply.github.com>
2022-07-10 18:30:09 +0800

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,6 @@
+CUDA_VISIBLE_DEVICES=3 python run_nerf.py --config configs/mvi1.txt
+CUDA_VISIBLE_DEVICES=3 python run_nerf.py --config configs/mvi2.txt
+CUDA_VISIBLE_DEVICES=3 python run_nerf.py --config configs/mvi3.txt
+CUDA_VISIBLE_DEVICES=3 python run_nerf.py --config configs/mvi4.txt
+CUDA_VISIBLE_DEVICES=3 python run_nerf.py --config configs/mvi5.txt
+CUDA_VISIBLE_DEVICES=3 python run_nerf.py --config configs/mvi6.txt
